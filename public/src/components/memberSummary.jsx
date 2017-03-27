4ae3e41d5a125cf70d5f27f173c4477e00fceb04
Gary Wong <gary.wonghg@gmail.com>
2017-03-26 18:32:10 -0700

    Update query function for multiple receipts

--- public/src/components/memberSummary.jsx
+++ public/src/components/memberSummary.jsx
@@ -20,61 +20,6 @@ class MemberSummary extends React.Component {
   
 
   handleSubmit(event) {
-    // TODO: the dummyData will be passed down from MemberSummary's parent component
-    // let dummyData = {
-    //   "isAuthenticated": true,
-    //   "tripName": "Japan2016",
-    //   "username": "Gary Wong",
-    //   "tripDesc": "",
-    //   "receiptName": "Receipt01",
-    //   "receiptUrl": "google.com/receipt01.jpg",
-    //   "items": [
-    //     [
-    //       {
-    //         "name": "Pizza",
-    //         "amount": "15",
-    //         "members": [
-    //           "Duy Nguyen"
-    //         ]
-    //       }
-    //     ],
-    //     [
-    //       {
-    //         "name": "Candy",
-    //         "amount": "20",
-    //         "members": [
-    //           "Gary",
-    //           "Whitney"
-    //         ]
-    //       }
-    //     ]
-    //   ],
-    //   "selectItem": 3,
-    //   "selectMember": "Gary Wong",
-    //   "members": [
-    //     [
-    //       "Duy Nguyen"
-    //     ],
-    //     [
-    //       "Gary"
-    //     ],
-    //     [
-    //       "Whitney"
-    //     ],
-    //     [
-    //       "Eugene"
-    //     ]
-    //   ],
-    //   sumBill: 100,
-    //   sumTax: 15,
-    //   sumTip: 20,
-    //   "member": "",
-    //   "memberExist": false,
-    //   "name": "",
-    //   "amount": "",
-    //   "fb_id": "10155155720694140"
-    // }
-
     event.preventDefault();
     // this.setState({dummyData});
     Util.insertIntoDb(this.props.data);
